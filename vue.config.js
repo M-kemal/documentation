059418be675a9a0662820b6bd26e02d728372093
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2024-01-14 21:09:39 +0300

    updatte

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/documentation/" : "/",
+// };
+
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: process.env.NODE_ENV !== "development" ? "/documentation" : "/",
 });
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/documentation/" : "/",
-};
